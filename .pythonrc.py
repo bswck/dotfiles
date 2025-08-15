5343f98486e329e210e027676e895dacef8b6871
bswck <bartosz@slawecki.dev>
2025-08-15 03:16:11 +0200

    Make `~/.pythonrc.py` more lenient

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,13 +1,25 @@
+import importlib.util as _importlib_util
 import os as _os
-from pythonrc_manager import (
-    DisplayHookPatcher as _DisplayHookPatcher,
-    init_rc_script as _init_rc_script,
-    project_rc_path as _project_rc_path,
-)
 
-_displayhook = _DisplayHookPatcher.pprinting()
-_displayhook.start()
 
-_rc_path = _project_rc_path()
-if _os.path.exists(_rc_path):
-    _init_rc_script(_rc_path(), globals())
+def pdir(o) -> list[str]:
+    return [o for o in dir(o) if not o.startswith("_")]
+
+
+if _importlib_util.find_spec("pythonrc_manager"):
+    from pythonrc_manager import (
+        DisplayHookPatcher as _DisplayHookPatcher,
+    )
+    from pythonrc_manager import (
+        init_rc_script as _init_rc_script,
+    )
+    from pythonrc_manager import (
+        project_rc_path as _project_rc_path,
+    )
+
+    _displayhook = _DisplayHookPatcher.pprinting()
+    _displayhook.start()
+
+    _rc_path = _project_rc_path()
+    if _os.path.exists(_rc_path):
+        _init_rc_script(_rc_path, globals())
