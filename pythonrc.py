143b085e5e5415aab2e8a1ceeaa862a725b90a40
bswck <bartosz@slawecki.dev>
2025-08-18 13:52:16 +0200

    Update `pythonrc.py`

--- pythonrc.py
+++ pythonrc.py
@@ -1,6 +1,8 @@
 import importlib.util as _importlib_util
 import os as _os
+import sys
 
+_displayhook = sys.displayhook
 
 def pdir(o) -> list[str]:
     return [o for o in dir(o) if not o.startswith("_")]
@@ -20,6 +22,7 @@ if _importlib_util.find_spec("pythonrc_manager"):
     _displayhook = _DisplayHookPatcher.pprinting()
     _displayhook.start()
 
-    _rc_path = _project_rc_path()
-    if _os.path.exists(_rc_path):
-        _init_rc_script(_rc_path, globals())
+    if __name__ == "__main__":
+        _rc_path = _project_rc_path()
+        if _os.path.exists(_rc_path):
+            _init_rc_script(_rc_path, globals())
