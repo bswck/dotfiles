bebf3a55ee98f685f2b882744f8f8b31fbd39f8f
bswck <bartosz@slawecki.dev>
2025-09-06 14:18:40 +0200

    Bunch of fixes

--- pythonrc.py
+++ pythonrc.py
@@ -7,7 +7,6 @@ _displayhook = sys.displayhook
 def pdir(o) -> list[str]:
     return [o for o in dir(o) if not o.startswith("_")]
 
-
 if _importlib_util.find_spec("pythonrc_manager"):
     from pythonrc_manager import (
         DisplayHookPatcher as _DisplayHookPatcher,
@@ -18,12 +17,18 @@ if _importlib_util.find_spec("pythonrc_manager"):
     from pythonrc_manager import (
         project_rc_path as _project_rc_path,
     )
-    from pythonrc_manager import restart
 
-    _displayhook = _DisplayHookPatcher.pprinting()
+    try:
+        import rich.pretty as rich_pretty
+        _pprinter = rich_pretty.pprint
+    except ImportError:
+        import pprint
+        _pprinter = pprint.pprint
+
+    _displayhook = _DisplayHookPatcher(_pprinter)
     _displayhook.start()
 
     if __name__ == "__main__":
         _rc_path = _project_rc_path()
-        if _os.path.exists(_rc_path):
+        if _rc_path and _os.path.exists(_rc_path):
             _init_rc_script(_rc_path, globals())
