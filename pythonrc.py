16f126d9bf5530e77ab10ae62052afa3f6eef9e1
bswck <bartosz@slawecki.dev>
2025-09-14 14:10:38 +0200

    Bundle `pythonrc_manager`
    Closes #1

--- pythonrc.py
+++ pythonrc.py
@@ -1,37 +1,28 @@
-import importlib.util as _importlib_util
-import os as _os
+import os
 import sys
+from pythonrc_manager import DisplayHookPatcher as _DisplayHookPatcher
+from pythonrc_manager import init_rc_script as _init_rc_script
+from pythonrc_manager import project_rc_path as _project_rc_path
 
-_displayhook = sys.displayhook
-
-def pdir(o) -> list[str]:
+def pdir(o: object) -> list[str]:
     return [o for o in dir(o) if not o.startswith("_")]
 
-def pvars(o) -> dict[str, object]:
+def pvars(o: object) -> dict[str, object]:
     return {k: v for k, v in vars(o).items() if not k.startswith("_")}
 
-if _importlib_util.find_spec("pythonrc_manager"):
-    from pythonrc_manager import (
-        DisplayHookPatcher as _DisplayHookPatcher,
-    )
-    from pythonrc_manager import (
-        init_rc_script as _init_rc_script,
-    )
-    from pythonrc_manager import (
-        project_rc_path as _project_rc_path,
-    )
+try:
+    from rich.pretty import pprint as rich_pprint
+    do_pprint = rich_pprint
+except ImportError:
+    from pprint import pprint as pprint_pprint
+    do_pprint = pprint_pprint
 
-    try:
-        import rich.pretty as rich_pretty
-        _pprinter = rich_pretty.pprint
-    except ImportError:
-        import pprint
-        _pprinter = pprint.pprint
+_dp = _DisplayHookPatcher(do_pprint)  # 🦈
+_dp.start()
 
-    _displayhook = _DisplayHookPatcher(_pprinter)
-    _displayhook.start()
+d = sys.displayhook
 
-    if __name__ == "__main__":
-        _rc_path = _project_rc_path()
-        if _rc_path and _os.path.exists(_rc_path):
-            _init_rc_script(_rc_path, globals())
+if __name__ == "__main__":
+    _rc = _project_rc_path()
+    if _rc and os.path.exists(_rc):
+        _init_rc_script(_rc, globals())
